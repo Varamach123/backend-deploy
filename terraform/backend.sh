2edb91bb5e80608752caf39a18449bae48f834ff
Varamach123 <chsrikarvarma9@gmail.com>
2024-07-26 14:12:18 +0530

    ped

--- terraform/backend.sh
+++ terraform/backend.sh
@@ -4,4 +4,4 @@ environment=$2
 app_Version=$3
 dnf install ansible -y
 pip3.9 install botocore boto3
-ansible-pull -i localhost, -U https://github.com/Varamach123/expense-ansible-roles-tf1.git main.yaml -e component=$component -e env=$environment -e appVersion=$app_Version
+ansible-pull -i localhost, -U https://github.com/Varamach123/expense-ansible-roles-tf1.git main.yaml -e component=$component -e env=$environment -e appVersion=$3
